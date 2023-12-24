7461011adcb338aa641d069a17e581aa32ae24d3
Saransh Singh <saranshsingh.it2021@nsec.ac.in>
2023-12-24 20:37:53 +0530

    Before motion

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
 import { HiOutlineDownload } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
 
-
-function Card() {
+function Card({data}) {
   return (
-    <div className='relative w-60 h-72 rounded-[35px] bg-zinc-900/90 text-white px-5 py-10 overflow-hidden'>
+    <div className='flex-shrink-0 relative w-60 h-72 rounded-[35px] bg-zinc-900/90 text-white px-5 py-10 overflow-hidden'>
         <FaRegFileAlt />
 
-        <p className='text-sm mt-5 font-semibold leading-tight'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati.</p>
+        <p className='text-sm mt-5 font-semibold leading-tight'>{data.desc}</p>
 
         <div className="footer absolute bottom-0 left-0 w-full">
-            <div className='flex items-center justify-between px-8 mb-3 py-3'>
-                <h5>.4mb</h5>
+            <div className='flex items-center justify-between px-8 mb-3 py-4'>
+                <h5>{data.filesize}</h5>
                 <span className='w-7 h-7 rounded-full bg-zinc-600 flex items-center justify-center'>
-                    <HiOutlineDownload size=".8em" color='#fff'/>
+                    {data.close ? <IoClose /> : <HiOutlineDownload size=".8em" color='#fff'/>}
+                    
                 </span>
             </div>
-            <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
-                <h3 className='text-sm font-semibold'>Download Now</h3>
-            </div>
+            {data.tag.isOpen && (
+                <div className={`tag w-full py-4 ${data.tag.tagColor} flex items-center justify-center`}>
+                <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
+                </div>
+            )}
         </div>
     </div>
   )
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
-import Card from './Card'
+import React from "react";
+import Card from "./Card";
+import { IoClose } from "react-icons/io5";
+
 function Foreground() {
+  const data = [
+        {
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati.",
+            filesize: ".4mb",
+            close: false,
+            tag: { isOpen:true, tagTitle:"Download Now", tagColor:"bg-green-600" },
+        },
+        {
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati.",
+            filesize: ".9mb",
+            close: false,
+            tag: { isOpen:true, tagTitle:"Download Now", tagColor:"bg-blue-600" },
+        },
+        {
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati.",
+            filesize: "1mb",
+            close: true,
+            tag: { isOpen:false, tagTitle:"Upload", tagColor:"bg-green-600" },
+        },
+    ];
+
   return (
-    <div className="fixed top-0 left-0 w-full h-screen z-[3] ">
-        <Card/>
+    <div className="fixed top-0 left-0 w-full h-screen z-[3] flex flex-wrap gap-10 p-5">
+      {data.map((item,index)=>(
+        <Card data={item}/>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Foreground
+export default Foreground;
